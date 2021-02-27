745cda3d9f23a7ba650f12575a2baf32e8946b7e
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2021-02-27 18:26:11 -0300

    visualizado funcionamento da função listOf, sortedBy e groupBy

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,36 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("Jose", 600.2, "PJ")
+    val maria = Funcionario("maria", 100.2, "PF")
+    val jullio = Funcionario("jullio", 500.2, "PJ")
+
+    val funcionarios = listOf(joao, maria, jullio)
+
+    funcionarios.forEach { println(it) }
+
+    println("-----------")
+    println(funcionarios.find { it.nome == "maria" })
+
+    println("-----------")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("-----------")
+    funcionarios.groupBy{ it.contratado}.forEach { println(it) }
+
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val contratado: String
+){
+    override fun toString(): String =
+        """
+            Nome:       $nome
+            Salario:    $salario
+            comtrato:   $contratado
+            
+        """.trimIndent()
+}
--- src/main/kotlin/one/digitalinnovation/collections/TesteOPeracoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOPeracoes.kt
@@ -1,12 +1,29 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val salarios = doubleArrayOf(600.00, 200.00, 100.0)
+    val salarios = doubleArrayOf(600.00, 200.00, 100.0, 5000.0, 3500.0)
 
     for (salario  in salarios){
         println(salario)
     }
 
     println("-------------")
-    println("Marios salário: $(salarios.max())")
+    println("Maior salário: ${salarios.maxOrNull()}")
+    println("Menor salário: ${salarios.minOrNull()}")
+    println("Media salário: ${salarios.average()}")
+
+    val salarioMaior2500 = salarios.filter { it > 2500 }
+    println("----------------")
+    salarioMaior2500.forEach{ println(it)}
+
+    println("-------------")
+    println(salarios.count{it in 2000.0..5000.0})
+
+    println("-------------")
+    println(salarios.find{it == 200.0})
+    println(salarios.find{it == 700.0})
+
+    println("-------------")
+    println(salarios.any{it == 600.0})
+    println(salarios.any{it == 300.0})
 }
