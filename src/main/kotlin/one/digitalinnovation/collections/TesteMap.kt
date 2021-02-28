2ddddf884fd628a2a2b8c983ac1537a561c505c8
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2021-02-28 14:39:56 -0300

    Criado class Texte Map e interagido com a função

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.collections
+
+fun main() {
+
+    val pair: Pair<String, Double> = Pair("Joao", 1000.0)
+    val map1 = mapOf(pair)
+
+    map1.forEach{ (k, v) -> println("Chave: $k - valor: $v")}
+
+    println("----------------------------")
+    val map2 = mapOf(
+        "Pedro" to 2500.0,
+        "Maria" to 3500.0
+    )
+    
+    map2.forEach { (k, v) -> println("Chave: $k - valor: $v") }
+}
